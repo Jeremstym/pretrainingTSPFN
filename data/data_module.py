e60d0158213e9eda2f3d9ab43801365c69fd9fec
Jeremstym <jeremstym@gmail.com>
2025-11-18 14:09:44 +0100

    Monitor df

--- data/data_module.py
+++ data/data_module.py
@@ -61,6 +61,7 @@ class TSPFNDataset(Dataset):
                 pbar.update(chunk.shape[0])
 
         df = pd.concat(list_df, ignore_index=True)
+        print(f"df is {df}")
         print(f"Unique labels before encoding in {name_csv}: {df.iloc[:, -1].unique()}")
         # Encode labels to integers
         df.iloc[:, -1] = self.label_encoder.fit_transform(df.iloc[:, -1])
