c2bfcaba42cc98b165bc9525f21334fe19bbe88b
Jeremstym <jeremstym@gmail.com>
2025-11-27 21:57:15 +0100

    Monitor batch shape

--- tspfn/pretraining/tspfn_module.py
+++ tspfn/pretraining/tspfn_module.py
@@ -196,6 +196,10 @@ class TSPFNPretraining(TSPFNSystem):
             y_batch_support = torch.cat(y_batch_support_list, dim=0).to(self.device)
             y_batch_query = torch.cat(y_batch_query_list, dim=0).to(self.device)
 
+            print(f"ts shape after split: {ts.shape}")
+            print(f"y_batch_support shape after split: {y_batch_support.shape}")
+            print(f"y_batch_query shape after split: {y_batch_query.shape}")
+
         # if self.training or len(self.y_train_for_inference) == 0:
         #     y_batch_support = torch.as_tensor(y[train_indices], dtype=torch.float32).to(self.device)
         #     y_batch_query = torch.as_tensor(y[test_indices], dtype=torch.float32).to(self.device)
