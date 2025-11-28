c1cb64f173e842fef34cd705643853cb414587c9
Jeremstym <jeremstym@gmail.com>
2025-11-28 17:05:28 +0100

    New import module

--- data/data_module.py
+++ data/data_module.py
@@ -22,7 +22,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from pytorch_lightning.trainer.supporters import CombinedLoader
+from pytorch_lightning.trainer import CombinedLoader
 
 
 class TSPFNDataset(Dataset):
