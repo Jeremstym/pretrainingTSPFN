22b60a2a1333096cdb56e6c61081ab0a72d4702c
Jeremstym <jeremstym@gmail.com>
2026-01-07 18:14:02 +0100

    Add gpu on script

--- scripts/dataprocessing.sh
+++ scripts/dataprocessing.sh
@@ -2,6 +2,7 @@
 #SBATCH --partition=electronic,hard
 #SBATCH --job-name=TUEV-processing
 #SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
 #SBATCH --time=1-23:00:00
 #SBATCH --output=/home/stympopper/bash/out/%x-%j.out
 #SBATCH --error=/home/stympopper/bash/out/%x-%j.err
