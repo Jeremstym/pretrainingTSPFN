d5aea55f471dc6565231660fdb855648fdf25afd
Jeremstym <jeremstym@gmail.com>
2026-01-08 16:33:48 +0100

    Add TUAB and TUEV datasets for evaluation

---
+++ data/evaluation_datasets.py
@@ -0,0 +1,128 @@
+# --------------------------------------------------------
+# Based on LaBraM, BEiT-v2, timm, DeiT, DINO, and BIOT code bases
+# https://github.com/935963004/LaBraM
+# https://github.com/microsoft/unilm/tree/master/beitv2
+# https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# https://github.com/facebookresearch/deit/
+# https://github.com/facebookresearch/dino
+# https://github.com/ycq091044/BIOT
+# ---------------------------------------------------------
+
+import io
+import os
+import math
+import time
+import json
+import glob
+from collections import defaultdict, deque
+import datetime
+import numpy as np
+from timm.utils import get_state_dict
+
+from pathlib import Path
+import argparse
+
+import torch
+import torch.distributed as dist
+from torch import inf
+import h5py
+
+import pickle
+from scipy.signal import resample
+import pandas as pd
+
+standard_1020 = [
+    "FP1-F7",
+    "F7-T7",
+    "T7-P7",
+    "P7-O1",
+    "FP2-F8",
+    "F8-T8",
+    "T8-P8",
+    "P8-O2",
+    "FP1-F3",
+    "F3-C3",
+    "C3-P3",
+    "P3-O1",
+    "FP2-F4",
+    "F4-C4",
+    "C4-P4",
+    "P4-O2",
+]
+
+
+class TUABLoader(torch.utils.data.Dataset):
+    def __init__(self, root, files, sampling_rate=200):
+        self.root = root
+        self.files = files
+        self.default_rate = 200
+        self.sampling_rate = sampling_rate
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        sample = pickle.load(open(os.path.join(self.root, self.files[index]), "rb"))
+        X = sample["X"]
+        if self.sampling_rate != self.default_rate:
+            X = resample(X, 10 * self.sampling_rate, axis=-1)
+        Y = sample["y"]
+        X = torch.FloatTensor(X)
+        return X, Y
+
+
+class TUEVLoader(torch.utils.data.Dataset):
+    def __init__(self, root, files, sampling_rate=200):
+        self.root = root
+        self.files = files
+        self.default_rate = 200
+        self.sampling_rate = sampling_rate
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        sample = pickle.load(open(os.path.join(self.root, self.files[index]), "rb"))
+        X = sample["signal"]
+        if self.sampling_rate != self.default_rate:
+            X = resample(X, 5 * self.sampling_rate, axis=-1)
+        Y = int(sample["label"][0] - 1)
+        X = torch.FloatTensor(X)
+        return X, Y
+
+
+def prepare_TUEV_dataset(root):
+    # set random seed
+    seed = 4523
+    np.random.seed(seed)
+
+    train_files = os.listdir(os.path.join(root, "processed_train"))
+    val_files = os.listdir(os.path.join(root, "processed_eval"))
+    test_files = os.listdir(os.path.join(root, "processed_test"))
+
+    # prepare training and test data loader
+    train_dataset = TUEVLoader(os.path.join(root, "processed_train"), train_files)
+    test_dataset = TUEVLoader(os.path.join(root, "processed_test"), test_files)
+    val_dataset = TUEVLoader(os.path.join(root, "processed_eval"), val_files)
+    print(len(train_files), len(val_files), len(test_files))
+    return train_dataset, test_dataset, val_dataset
+
+
+def prepare_TUAB_dataset(root):
+    # set random seed
+    seed = 12345
+    np.random.seed(seed)
+
+    train_files = os.listdir(os.path.join(root, "train"))
+    np.random.shuffle(train_files)
+    val_files = os.listdir(os.path.join(root, "val"))
+    test_files = os.listdir(os.path.join(root, "test"))
+
+    print(len(train_files), len(val_files), len(test_files))
+
+    # prepare training and test data loader
+    train_dataset = TUABLoader(os.path.join(root, "train"), train_files)
+    test_dataset = TUABLoader(os.path.join(root, "test"), test_files)
+    val_dataset = TUABLoader(os.path.join(root, "val"), val_files)
+    print(len(train_files), len(val_files), len(test_files))
+    return train_dataset, test_dataset, val_dataset
