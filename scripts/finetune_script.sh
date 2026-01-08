ebbac1f502281f6f5729208300a57141861b8f08
Jeremstym <jeremstym@gmail.com>
2026-01-08 18:43:59 +0100

    Script finetune

---
+++ scripts/finetune_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=hard
+#SBATCH --job-name=TUEV-processing
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=1-23:00:00
+#SBATCH --output=/home/stympopper/bash/out/%x-%j.out
+#SBATCH --error=/home/stympopper/bash/out/%x-%j.err
+
+uname -a
+nvidia-smi
+
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/TUEV-zeroshot/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning seed=42 updated_pfn_path=/home/stympopper/didacticJerem/ckpts/tspfn_encoder_weights_v2.pt
