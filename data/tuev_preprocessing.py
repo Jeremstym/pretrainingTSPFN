4b615c8c1b8d0e7a9099ca2895b19040bdaed27d
Jeremstym <jeremstym@gmail.com>
2026-01-11 20:47:53 +0100

    Full dataset making no filtered

--- data/tuev_preprocessing.py
+++ data/tuev_preprocessing.py
@@ -14,20 +14,68 @@ from tqdm import tqdm
 https://github.com/Abhishaike/EEG_Event_Classification
 """
 
-drop_channels = ['PHOTIC-REF', 'IBI', 'BURSTS', 'SUPPR', 'EEG ROC-REF', 'EEG LOC-REF', 'EEG EKG1-REF', 'EMG-REF', 'EEG C3P-REF', 'EEG C4P-REF', 'EEG SP1-REF', 'EEG SP2-REF', \
-                 'EEG LUC-REF', 'EEG RLC-REF', 'EEG RESP1-REF', 'EEG RESP2-REF', 'EEG EKG-REF', 'RESP ABDOMEN-REF', 'ECG EKG-REF', 'PULSE RATE', 'EEG PG2-REF', 'EEG PG1-REF']
-drop_channels.extend([f'EEG {i}-REF' for i in range(20, 129)])
-chOrder_standard = ['EEG FP1-REF', 'EEG FP2-REF', 'EEG F3-REF', 'EEG F4-REF', 'EEG C3-REF', 'EEG C4-REF', 'EEG P3-REF', 'EEG P4-REF', 'EEG O1-REF', 'EEG O2-REF', 'EEG F7-REF', \
-                    'EEG F8-REF', 'EEG T3-REF', 'EEG T4-REF', 'EEG T5-REF', 'EEG T6-REF', 'EEG A1-REF', 'EEG A2-REF', 'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF', 'EEG T1-REF', 'EEG T2-REF']
+drop_channels = [
+    "PHOTIC-REF",
+    "IBI",
+    "BURSTS",
+    "SUPPR",
+    "EEG ROC-REF",
+    "EEG LOC-REF",
+    "EEG EKG1-REF",
+    "EMG-REF",
+    "EEG C3P-REF",
+    "EEG C4P-REF",
+    "EEG SP1-REF",
+    "EEG SP2-REF",
+    "EEG LUC-REF",
+    "EEG RLC-REF",
+    "EEG RESP1-REF",
+    "EEG RESP2-REF",
+    "EEG EKG-REF",
+    "RESP ABDOMEN-REF",
+    "ECG EKG-REF",
+    "PULSE RATE",
+    "EEG PG2-REF",
+    "EEG PG1-REF",
+]
+drop_channels.extend([f"EEG {i}-REF" for i in range(20, 129)])
+chOrder_standard = [
+    "EEG FP1-REF",
+    "EEG FP2-REF",
+    "EEG F3-REF",
+    "EEG F4-REF",
+    "EEG C3-REF",
+    "EEG C4-REF",
+    "EEG P3-REF",
+    "EEG P4-REF",
+    "EEG O1-REF",
+    "EEG O2-REF",
+    "EEG F7-REF",
+    "EEG F8-REF",
+    "EEG T3-REF",
+    "EEG T4-REF",
+    "EEG T5-REF",
+    "EEG T6-REF",
+    "EEG A1-REF",
+    "EEG A2-REF",
+    "EEG FZ-REF",
+    "EEG CZ-REF",
+    "EEG PZ-REF",
+    "EEG T1-REF",
+    "EEG T2-REF",
+]
 
 
 def BuildEvents(signals, times, EventData, keep_channels):
     # Filter EventData to only include rows where the channel is in keep_channels
-    mask = np.isin(EventData[:, 0], keep_channels)
-    filtered_EventData = EventData[mask]
-    numEvents = len(filtered_EventData)
+    # mask = np.isin(EventData[:, 0], keep_channels)
+    # filtered_EventData = EventData[mask]
+    # numEvents = len(filtered_EventData)
+    [numEvents, _] = EventData.shape
     fs = 200.0
-    features = np.zeros([numEvents, len(keep_channels), int(fs) * 5])
+    # features = np.zeros([numEvents, len(keep_channels), int(fs) * 5])
+    [numChan, numPoints] = signals.shape
+    features = np.zeros([numEvents, numChan, int(fs) * 5])
     # # Replace the triple concatenation with padding
     # # Pad only the time axis (axis 1) with 2 seconds worth of samples
     # pad_width = int(fs) * 2
@@ -42,86 +90,36 @@ def BuildEvents(signals, times, EventData, keep_channels):
     signals = np.concatenate([signals, signals, signals], axis=1)
     for i in range(numEvents):  # for each event
         chan = int(filtered_EventData[i, 0])  # chan is channel
-        if chan not in keep_channels:
-            raise Exception("Channel not in keep_channels")
+        # if chan not in keep_channels:
+        #     raise Exception("Channel not in keep_channels")
         start = np.where((times) >= filtered_EventData[i, 1])[0][0]
         end = np.where((times) >= filtered_EventData[i, 2])[0][0]
         # print (offset + start - 2 * int(fs), offset + end + 2 * int(fs), signals.shape)
-        features[i, :] = signals[
-            :, offset + start - 2 * int(fs) : offset + end + 2 * int(fs)
-        ]
+        features[i, :] = signals[:, offset + start - 2 * int(fs) : offset + end + 2 * int(fs)]
         offending_channel[i, :] = int(chan)
         labels[i, :] = int(filtered_EventData[i, 3])
     return [features, offending_channel, labels]
 
 
 def convert_signals(signals, Rawdata):
-    signal_names = {
-        k: v
-        for (k, v) in zip(
-            Rawdata.info["ch_names"], list(range(len(Rawdata.info["ch_names"])))
-        )
-    }
+    signal_names = {k: v for (k, v) in zip(Rawdata.info["ch_names"], list(range(len(Rawdata.info["ch_names"]))))}
     new_signals = np.vstack(
         (
-            signals[signal_names["EEG FP1-REF"]]
-            - signals[signal_names["EEG F7-REF"]],  # 0
-            (
-                signals[signal_names["EEG F7-REF"]]
-                - signals[signal_names["EEG T3-REF"]]
-            ),  # 1
-            (
-                signals[signal_names["EEG T3-REF"]]
-                - signals[signal_names["EEG T5-REF"]]
-            ),  # 2
-            (
-                signals[signal_names["EEG T5-REF"]]
-                - signals[signal_names["EEG O1-REF"]]
-            ),  # 3
-            (
-                signals[signal_names["EEG FP2-REF"]]
-                - signals[signal_names["EEG F8-REF"]]
-            ),  # 4
-            (
-                signals[signal_names["EEG F8-REF"]]
-                - signals[signal_names["EEG T4-REF"]]
-            ),  # 5
-            (
-                signals[signal_names["EEG T4-REF"]]
-                - signals[signal_names["EEG T6-REF"]]
-            ),  # 6
-            (
-                signals[signal_names["EEG T6-REF"]]
-                - signals[signal_names["EEG O2-REF"]]
-            ),  # 7
-            (
-                signals[signal_names["EEG FP1-REF"]]
-                - signals[signal_names["EEG F3-REF"]]
-            ),  # 14
-            (
-                signals[signal_names["EEG F3-REF"]]
-                - signals[signal_names["EEG C3-REF"]]
-            ),  # 15
-            (
-                signals[signal_names["EEG C3-REF"]]
-                - signals[signal_names["EEG P3-REF"]]
-            ),  # 16
-            (
-                signals[signal_names["EEG P3-REF"]]
-                - signals[signal_names["EEG O1-REF"]]
-            ),  # 17
-            (
-                signals[signal_names["EEG FP2-REF"]]
-                - signals[signal_names["EEG F4-REF"]]
-            ),  # 18
-            (
-                signals[signal_names["EEG F4-REF"]]
-                - signals[signal_names["EEG C4-REF"]]
-            ),  # 19
-            (
-                signals[signal_names["EEG C4-REF"]]
-                - signals[signal_names["EEG P4-REF"]]
-            ),  # 20
+            signals[signal_names["EEG FP1-REF"]] - signals[signal_names["EEG F7-REF"]],  # 0
+            (signals[signal_names["EEG F7-REF"]] - signals[signal_names["EEG T3-REF"]]),  # 1
+            (signals[signal_names["EEG T3-REF"]] - signals[signal_names["EEG T5-REF"]]),  # 2
+            (signals[signal_names["EEG T5-REF"]] - signals[signal_names["EEG O1-REF"]]),  # 3
+            (signals[signal_names["EEG FP2-REF"]] - signals[signal_names["EEG F8-REF"]]),  # 4
+            (signals[signal_names["EEG F8-REF"]] - signals[signal_names["EEG T4-REF"]]),  # 5
+            (signals[signal_names["EEG T4-REF"]] - signals[signal_names["EEG T6-REF"]]),  # 6
+            (signals[signal_names["EEG T6-REF"]] - signals[signal_names["EEG O2-REF"]]),  # 7
+            (signals[signal_names["EEG FP1-REF"]] - signals[signal_names["EEG F3-REF"]]),  # 14
+            (signals[signal_names["EEG F3-REF"]] - signals[signal_names["EEG C3-REF"]]),  # 15
+            (signals[signal_names["EEG C3-REF"]] - signals[signal_names["EEG P3-REF"]]),  # 16
+            (signals[signal_names["EEG P3-REF"]] - signals[signal_names["EEG O1-REF"]]),  # 17
+            (signals[signal_names["EEG FP2-REF"]] - signals[signal_names["EEG F4-REF"]]),  # 18
+            (signals[signal_names["EEG F4-REF"]] - signals[signal_names["EEG C4-REF"]]),  # 19
+            (signals[signal_names["EEG C4-REF"]] - signals[signal_names["EEG P4-REF"]]),  # 20
             (signals[signal_names["EEG P4-REF"]] - signals[signal_names["EEG O2-REF"]]),
         )
     )  # 21
@@ -148,7 +146,7 @@ def readEDF(fileName):
     Rawdata.resample(200, n_jobs=5)
 
     _, times = Rawdata[:]
-    signals = Rawdata.get_data(units='uV')
+    signals = Rawdata.get_data(units="uV")
     RecFile = fileName[0:-3] + "rec"
     eventData = np.genfromtxt(RecFile, delimiter=",")
     Rawdata.close()
@@ -165,14 +163,13 @@ def load_up_objects(BaseDir, Features, OffendingChannels, Labels, OutDir):
                     [signals, times, event, Rawdata] = readEDF(
                         dirName + "/" + fname
                     )  # event is the .rec file in the form of an array
-                    signals, keep_channels = convert_signals(signals, Rawdata)
+                    # signals, keep_channels = convert_signals(signals, Rawdata)
+                    keep_channels = list(range(len(chOrder_standard)))
                 except (ValueError, KeyError):
                     print("something funky happened in " + dirName + "/" + fname)
                     continue
                 signals, offending_channels, labels = BuildEvents(signals, times, event, keep_channels)
-                for idx, (signal, offending_channel, label) in enumerate(
-                    zip(signals, offending_channels, labels)
-                ):
+                for idx, (signal, offending_channel, label) in enumerate(zip(signals, offending_channels, labels)):
                     sample = {
                         "signal": signal,
                         "offending_channel": offending_channel,
@@ -180,9 +177,7 @@ def load_up_objects(BaseDir, Features, OffendingChannels, Labels, OutDir):
                     }
                     save_pickle(
                         sample,
-                        os.path.join(
-                            OutDir, fname.split(".")[0] + "-" + str(idx) + ".pkl"
-                        ),
+                        os.path.join(OutDir, fname.split(".")[0] + "-" + str(idx) + ".pkl"),
                     )
 
     return Features, Labels, OffendingChannels
@@ -192,13 +187,14 @@ def save_pickle(object, filename):
     with open(filename, "wb") as f:
         pickle.dump(object, f)
 
+
 if __name__ == "__main__":
 
     """
     TUEV dataset is downloaded from https://isip.piconepress.com/projects/tuh_eeg/html/downloads.shtml
     """
 
-    root = "/data/stympopper/TUEV/edf"
+    root = "/data/stympopper/FullTUEV/edf"
     train_out_dir = os.path.join(root, "processed_train")
     eval_out_dir = os.path.join(root, "processed_eval")
     if not os.path.exists(train_out_dir):
@@ -208,29 +204,20 @@ if __name__ == "__main__":
 
     BaseDirTrain = os.path.join(root, "train")
     fs = 200
-    TrainFeatures = np.empty(
-        (0, 16, fs)
-    )  # 0 for lack of intialization, 22 for channels, fs for num of points
+    TrainFeatures = np.empty((0, 16, fs))  # 0 for lack of intialization, 22 for channels, fs for num of points
     TrainLabels = np.empty([0, 1])
     TrainOffendingChannel = np.empty([0, 1])
-    load_up_objects(
-        BaseDirTrain, TrainFeatures, TrainLabels, TrainOffendingChannel, train_out_dir
-    )
+    load_up_objects(BaseDirTrain, TrainFeatures, TrainLabels, TrainOffendingChannel, train_out_dir)
 
     BaseDirEval = os.path.join(root, "eval")
     fs = 200
-    EvalFeatures = np.empty(
-        (0, 16, fs)
-    )  # 0 for lack of intialization, 22 for channels, fs for num of points
+    EvalFeatures = np.empty((0, 16, fs))  # 0 for lack of intialization, 22 for channels, fs for num of points
     EvalLabels = np.empty([0, 1])
     EvalOffendingChannel = np.empty([0, 1])
-    load_up_objects(
-        BaseDirEval, EvalFeatures, EvalLabels, EvalOffendingChannel, eval_out_dir
-    )
+    load_up_objects(BaseDirEval, EvalFeatures, EvalLabels, EvalOffendingChannel, eval_out_dir)
 
-
-    #transfer to train, eval, and test
-    root = "/data/stympopper/TUEV/edf"
+    # transfer to train, eval, and test
+    root = "/data/stympopper/FullTUEV/edf"
     seed = 4523
     np.random.seed(seed)
 
@@ -239,15 +226,20 @@ if __name__ == "__main__":
     print("train sub", len(train_sub))
     test_files = os.listdir(os.path.join(root, "processed_eval"))
 
-    val_sub = np.random.choice(train_sub, size=int(
-        len(train_sub) * 0.2), replace=False)
+    val_sub = np.random.choice(train_sub, size=int(len(train_sub) * 0.2), replace=False)
     train_sub = list(set(train_sub) - set(val_sub))
     val_files = [f for f in train_files if f.split("_")[0] in val_sub]
     train_files = [f for f in train_files if f.split("_")[0] in train_sub]
 
     for file in train_files:
-        os.system(f"cp {os.path.join(root, 'processed_train', file)} {os.path.join(root, 'processed', 'processed_train')}")
+        os.system(
+            f"cp {os.path.join(root, 'processed_train', file)} {os.path.join(root, 'processed', 'processed_train')}"
+        )
     for file in val_files:
-        os.system(f"cp {os.path.join(root, 'processed_train', file)} {os.path.join(root, 'processed', 'processed_eval')}")
+        os.system(
+            f"cp {os.path.join(root, 'processed_train', file)} {os.path.join(root, 'processed', 'processed_eval')}"
+        )
     for file in test_files:
-        os.system(f"cp {os.path.join(root, 'processed_eval', file)} {os.path.join(root, 'processed', 'processed_test')}")
+        os.system(
+            f"cp {os.path.join(root, 'processed_eval', file)} {os.path.join(root, 'processed', 'processed_test')}"
+        )
