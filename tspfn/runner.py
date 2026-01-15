6c2af1dd94add183979658028d8ea8e49821b824
Jeremstym <jeremstym@gmail.com>
2026-01-15 13:28:03 +0100

    Put function in run system

--- tspfn/runner.py
+++ tspfn/runner.py
@@ -66,6 +66,7 @@ class TSPFNRunner(ABC):
         # `dataprocessing` default value of is `high`, the middle-ground between performance and precision. For more details:
         # https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
         torch.set_float32_matmul_precision(cfg.float32_matmul_precision)
+        torch.serialization.add_safe_globals([omegaconf.dictconfig.DictConfig])
 
         if cfg.ckpt:
             ckpt_path = resolve_model_checkpoint_path(cfg.ckpt)
