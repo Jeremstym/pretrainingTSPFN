d36fdbd883edb0f55e23295c1bd069296426e1e3
Jeremstym <jeremstym@gmail.com>
2026-01-16 20:21:46 +0100

    Try importing model

--- tspfn/foundationals/labram.py
+++ tspfn/foundationals/labram.py
@@ -4,4 +4,16 @@ from pathlib import Path
 # Add LaBraM to path before any labram-specific imports
 labram_path = Path("submodules/labram").resolve()
 sys.path.append(str(labram_path))
-#TEST
+
+from modeling_vqnsp import vqnsp_encoder_base_decoder_3x200x12
+
+if __name__ == "__main__":
+    model = vqnsp_encoder_base_decoder_3x200x12(
+        pretrained=True,
+        pretrained_weights="/home/stympopper/pretrainingTSPFN/ckpts/labram_vqnsp.pth",
+        as_tokenizer=True,
+        EEG_size=1600,
+        n_code=8192,
+        code_dim=32
+    )
+    print(model)
