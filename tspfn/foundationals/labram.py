6b973c7bc0a9c26c2890f5e61fc4b87a5b1bb30e
Jeremstym <jeremstym@gmail.com>
2026-01-16 20:43:54 +0100

    Print tokesn

--- tspfn/foundationals/labram.py
+++ tspfn/foundationals/labram.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         n_code=8192,
         code_dim=32
     )
-    x = torch.randn(4,128,8,200)
+    x = torch.randn(4,16,8,200)
     tokens = model.get_tokens(x)
-    print(tokens["quantize"].shape)
+    print(tokens["tokens"])
