13c6cd28a336cea05cd8711d4922ea63ebf143f1
Jeremstym <jeremstym@gmail.com>
2026-01-17 13:48:52 +0100

    Monitor patch embedding only

--- tspfn/foundationals/labram.py
+++ tspfn/foundationals/labram.py
@@ -44,24 +44,26 @@ class TimeSeriesNeuralTokenizer(torch.nn.Module):
         quantize = rearrange(quantize, "B D C A -> B (A C) D")
         return quantize.flatten(start_dim=1)
 
-# if __name__ == "__main__":
-#     model = vqnsp_encoder_base_decoder_3x200x12(
-#         pretrained=True,
-#         pretrained_weight="/home/stympopper/pretrainingTSPFN/ckpts/labram_vqnsp.pth",
-#         as_tokenzer=True,
-#         EEG_size=1000,
-#         n_code=8192,
-#         code_dim=64,
-#     )
-#     x = torch.randn(4, 16, 1000)
-#     x = rearrange(x, "B N (A T) -> B N A T", T=200)
-#     input_chans = list(range(x.size(1)+1))
-#     quantize, embed_ind, emb_loss = model.encode(x, input_chans=input_chans)
-#     # Random select channels
-#     indices = torch.randperm(quantize.size(-2))[:4]
-#     quantize = quantize[:, :, indices, :]
-#     print(f"quantize before rearrange is {quantize.shape}")
-#     quantize = rearrange(quantize, "B D C A -> B (A C) D")
-#     # print(tokens["token"].shape)
-#     print(f"token image is {embed_ind.view(x.size(0), -1).shape}")
-#     print(f"quantize is {quantize.flatten(start_dim=1).shape}")
+if __name__ == "__main__":
+    model = vqnsp_encoder_base_decoder_3x200x12(
+        pretrained=True,
+        pretrained_weight="/home/stympopper/pretrainingTSPFN/ckpts/labram_vqnsp.pth",
+        as_tokenzer=True,
+        EEG_size=1000,
+        n_code=8192,
+        code_dim=64,
+    )
+    x = torch.randn(4, 16, 1000)
+    x = rearrange(x, "B N (A T) -> B N A T", T=200)
+    x = model.encoder.patch_embed(x)
+    print(f"patch embed shape is {x.shape}")
+    # input_chans = list(range(x.size(1)+1))
+    # quantize, embed_ind, emb_loss = model.encode(x, input_chans=input_chans)
+    # # Random select channels
+    # indices = torch.randperm(quantize.size(-2))[:4]
+    # quantize = quantize[:, :, indices, :]
+    # print(f"quantize before rearrange is {quantize.shape}")
+    # quantize = rearrange(quantize, "B D C A -> B (A C) D")
+    # # print(tokens["token"].shape)
+    # print(f"token image is {embed_ind.view(x.size(0), -1).shape}")
+    # print(f"quantize is {quantize.flatten(start_dim=1).shape}")
