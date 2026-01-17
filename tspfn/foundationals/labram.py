243b12c4b8f7610506bfce5b2da3e0104df74590
Jeremstym <jeremstym@gmail.com>
2026-01-17 01:43:03 +0100

    Add one extra toke,

--- tspfn/foundationals/labram.py
+++ tspfn/foundationals/labram.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
         n_code=8192,
         code_dim=32
     )
-    x = torch.randn(4,16,1000)
+    x = torch.randn(4,17,1000)
     # Pad cls token
-    x = torch.cat([torch.zeros(x.size(0),1,x.size(2)), x], dim=1)  # (B, N+1, T)
     # tokens = model.get_tokens(x, input_chans=16)
     x = rearrange(x, 'B N (A T) -> B N A T', T=200)
     input_chans = list(range(x.size(1)))
