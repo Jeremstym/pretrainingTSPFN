44c9eede1a8b05100beb01dab9fdc0e196e0177c
Jeremstym <jeremstym@gmail.com>
2026-01-17 01:36:08 +0100

    Back to previous stat

--- tspfn/foundationals/labram.py
+++ tspfn/foundationals/labram.py
@@ -1,6 +1,5 @@
 import tspfn.foundationals
 import torch
-import torch.nn.functional as F
 from einops import rearrange
 from modeling_vqnsp import vqnsp_encoder_base_decoder_3x200x12
 
@@ -19,22 +18,9 @@ if __name__ == "__main__":
         n_code=8192,
         code_dim=32
     )
-    x = torch.randn(4,16,1600)
+    x = torch.randn(4,128,1600)
+    # tokens = model.get_tokens(x, input_chans=16)
     x = rearrange(x, 'B N (A T) -> B N A T', T=200)
-    # # tokens = model.get_tokens(x, input_chans=16)
-    # x = rearrange(x, 'B N (A T) -> B N A T', T=200)
-    # input_chans = list(range(16))
-    # quantize, embed_ind, emb_loss = model.encode(x, input_chans=input_chans)
-    # # print(tokens["token"].shape)
-    # print(f"token image is {embed_ind.view(x.size(0), -1).shape}")
-    with torch.no_grad():
-        # If your model object is already loaded with 128-ch weights:
-        # We pad to 128 just for the forward pass, then discard the rest.
-        x_padded = F.pad(x, (0, 0, 0, 112)) 
-        quantize, embed_ind, _ = model.encode(x_padded, input_chans=None)
-
-    # 3. Get exactly the 128 tokens you want (16 ch * 8 patches)
-    # embed_ind is shape [B, 1024]
-    indices_3d = embed_ind.view(x.size(0), 128, 8)
-    real_indices = indices_3d[:, :16, :] # Keep only the 16 channels you care about
-    final_tokens = real_indices.flatten(1) # Shape: [4, 128]
+    quantize, embed_ind, emb_loss = model.encode(x, input_chans=None)
+    # print(tokens["token"].shape)
+    print(f"token image is {embed_ind.view(x.size(0), -1).shape}")
