0cfc9a409bf0b4391823b5cf06ae7d481d047390
Jeremstym <jeremstym@gmail.com>
2026-01-17 02:05:43 +0100

    Safe load

--- tspfn/foundationals/labram.py
+++ tspfn/foundationals/labram.py
@@ -1,13 +1,18 @@
 import tspfn.foundationals
 import torch
+import numpy as np
 from einops import rearrange
 from modeling_vqnsp import vqnsp_encoder_base_decoder_3x200x12, vqnsp_encoder_large_decoder_3x200x24
+
+torch.serialization.add_safe_globals([np.core.multiarray.scalar])
+
 
 def std_norm(self, x):
     mean = torch.mean(x, dim=(1, 2, 3), keepdim=True)
     std = torch.std(x, dim=(1, 2, 3), keepdim=True)
     x = (x - mean) / std
     return x
+
 
 if __name__ == "__main__":
     model = vqnsp_encoder_large_decoder_3x200x24(
@@ -16,12 +21,12 @@ if __name__ == "__main__":
         as_tokenzer=True,
         EEG_size=1000,
         n_code=8192,
-        code_dim=32
+        code_dim=32,
     )
-    x = torch.randn(4,16,1000)
+    x = torch.randn(4, 16, 1000)
     # Pad cls token
     # tokens = model.get_tokens(x, input_chans=16)
-    x = rearrange(x, 'B N (A T) -> B N A T', T=200)
+    x = rearrange(x, "B N (A T) -> B N A T", T=200)
     input_chans = list(range(x.size(1)))
     quantize, embed_ind, emb_loss = model.encode(x, input_chans=input_chans)
     # print(tokens["token"].shape)
