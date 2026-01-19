97052d59ff48c6bf61b1acc04496da443dc74591
Jeremstym <jeremstym@gmail.com>
2026-01-19 15:27:50 +0100

    Fix name module

--- tspfn/finetuning/tspfn_finetune.py
+++ tspfn/finetuning/tspfn_finetune.py
@@ -33,7 +33,7 @@ from torchmetrics import MetricCollection
 from data.utils.decorators import auto_move_data
 from tspfn.system import TSPFNSystem
 from tspfn.foundationals.labram import TimeSeriesNeuralTokenizer
-from tspnf.foundationals.convolution import TimeSeriesConvolutionTokenizer
+from tspfn.foundationals.convolution import TimeSeriesConvolutionTokenizer
 from tspfn.utils import get_sizes_per_class, MulticlassFaiss, SingleclassFaiss, stratified_batch_split, half_batch_split
 
 logger = logging.getLogger(__name__)
