0529b5d1253cbd5e3f1606442ed8d5625719575d
Jeremstym <jeremstym@gmail.com>
2026-01-23 19:13:00 +0100

    Sort eicu data

--- scripts/dataprocessing.sh
+++ scripts/dataprocessing.sh
@@ -15,4 +15,5 @@ nvidia-smi
 # poetry run python ~/pretrainingTSPFN/data/tuev_preprocessing.py
 # poetry run python ~/pretrainingTSPFN/data/tuab_preprocessing.py
 # poetry run python ~/pretrainingTSPFN/data/tuev_cleaning.py
-poetry run python /data/stympopper/BenchmarkTSPFN/processed/EICU_preprocessed/eicu_makelabels.py
+# poetry run python /data/stympopper/BenchmarkTSPFN/processed/EICU_preprocessed/eicu_makelabels.py
+poetry run python /data/stympopper/BenchmarkTSPFN/processed/EICU_preprocessed/eicu_sort_ts.py
