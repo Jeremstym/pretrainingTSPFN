e78f03896960f594d3b584a351ddca4e7c84ee96
Jeremstym <jeremstym@gmail.com>
2026-01-27 20:22:09 +0100

    Script benchmark on ECG

---
+++ scripts/benchmark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=electronic,hard
+#SBATCH --job-name=TUEV-finetuning
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=8G
+#SBATCH --nodes=1
+#SBATCH --nodelist=kavinsky
+#SBATCH --gpus-per-node=1
+#SBATCH --time=1-23:00:00
+#SBATCH --output=/home/stympopper/bash/out/%x-%j.out
+#SBATCH --error=/home/stympopper/bash/out/%x-%j.err
+
+uname -a
+nvidia-smi
+ulimit -n 4096
+
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning seed=42 train=False test=True
