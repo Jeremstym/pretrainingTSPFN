e7b963426c4286274598f4dc2ba51db2a9604224
Jeremstym <jeremstym@gmail.com>
2026-01-27 21:27:24 +0100

    Use v2.5 for pretraining

--- scripts/pretrain_script.sh
+++ scripts/pretrain_script.sh
@@ -13,6 +13,8 @@ nvidia-smi
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/TRASH_TEST' +experiment=pretrainingTSPFN/tspfn-pretraining seed=42
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_BIGpretraining_v3' +experiment=pretrainingTSPFN/tspfn-pretraining seed=42
 
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_v2.5' +experiment=pretrainingTSPFN/tspfn-pretraining seed=42
+
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/BatchTraining' +experiment=pretrainingTSPFN/tspfn-pretraining seed=42
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/BatchTrainingNOHUGEDS' +experiment=pretrainingTSPFN/tspfn-pretraining seed=42
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ShortBatchTrainingNOHUGEDS' +experiment=pretrainingTSPFN/tspfn-pretraining seed=42 trainer.max_epochs=100
