4e0e53e25e0cf53cc4a27db606dc4f9399fdc84c
Jeremstym <jeremstym@gmail.com>
2026-01-29 00:07:49 +0100

    Remove doubliing

--- tspfn/baselines/xgboost.py
+++ tspfn/baselines/xgboost.py
@@ -95,17 +95,17 @@ class XGBoostStaticBaseline(pl.LightningModule):
         # Update metric collection
         self.val_metrics.update(y_probs_ts, y_eval.to(self.device))
 
-    def on_validation_epoch_end(self):
-        # Compute and log all metrics at once
-        output = self.val_metrics.compute()
-        self.log_dict(output, prog_bar=True)
-        self.val_metrics.reset()
-        # # Save CSV once at the very end
-        # with open("test_metrics.csv", mode="w", newline="") as csv_file:
-        #     writer = csv.DictWriter(csv_file, fieldnames=["metric", "value"])
-        #     writer.writeheader()
-        #     writer.writerows(output_data)
-        # logger.info("Test metrics saved to test_metrics.csv")
+    # def on_validation_epoch_end(self):
+    #     # Compute and log all metrics at once
+    #     output = self.val_metrics.compute()
+    #     self.log_dict(output, prog_bar=True)
+    #     self.val_metrics.reset()
+    #     # # Save CSV once at the very end
+    #     # with open("test_metrics.csv", mode="w", newline="") as csv_file:
+    #     #     writer = csv.DictWriter(csv_file, fieldnames=["metric", "value"])
+    #     #     writer.writeheader()
+    #     #     writer.writerows(output_data)
+    #     # logger.info("Test metrics saved to test_metrics.csv")
 
     def configure_optimizers(self):
         # Dummy optimizer since XGBoost isn't trained via SGD
