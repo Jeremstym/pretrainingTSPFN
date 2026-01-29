72322cd21dd83e8b7e7d03cd4538006f21423221
Jeremstym <jeremstym@gmail.com>
2026-01-29 12:20:42 +0100

    Use pfn path

--- tspfn/architecture/prediction_heads.py
+++ tspfn/architecture/prediction_heads.py
@@ -40,12 +40,11 @@ class PFNPredictionHead(nn.Module):
             # Load updated model weights after pretraining
             state_dict = torch.load(updated_pfn_path, map_location="cuda:0")
             new_state_dict = {}
-            for k, v in state_dict.items():
-                if k.startswith("model."):
-                    new_key = k[len("model."):]  # strip the prefix
-                    new_state_dict[new_key] = v
-                else:
-                    new_state_dict[k] = v
+            if k.startswith("model."):
+                new_key = k[len("model.") :]  # strip the prefix
+                new_state_dict[new_key] = v
+            else:
+                new_state_dict[k] = v
             model.load_state_dict(new_state_dict, strict=True)
 
         self.head = model.decoder_dict["standard"]
