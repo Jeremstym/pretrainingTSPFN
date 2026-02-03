18f48431ca963af373d91786e7575f27419dce89
Jeremstym <jeremstym@gmail.com>
2026-02-03 02:37:48 +0100

    Add kwargs

--- data/data_module.py
+++ data/data_module.py
@@ -234,6 +234,7 @@ class ECG5000FineTuneDataModule(TSPFNDataModule):
         pin_memory: bool = True,
         transform: Optional[Callable] = None,
         seed: int = 42,
+        **kwargs,
     ) -> None:
         super().__init__(
             data_roots=data_roots,
