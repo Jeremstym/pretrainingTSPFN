584349aabcfb0e1878e1ad64688b78fd719383e9
Jeremstym <jeremstym@gmail.com>
2026-02-07 00:26:57 +0100

    Monitor memory

--- tspfn/runner.py
+++ tspfn/runner.py
@@ -171,7 +171,9 @@ class TSPFNRunner(ABC):
                     assert cfg.ckpt is not None, "To resume training, a checkpoint path must be provided in cfg.ckpt"
                     trainer.fit(model, datamodule=datamodule, ckpt_path=ckpt_path)
                 else:
+                    print(f"VRAM allouée AVANT fit: {torch.cuda.memory_allocated() / 1e9:.2f} GB")
                     trainer.fit(model, datamodule=datamodule)
+                    print(f"VRAM réservée AVANT fit: {torch.cuda.memory_reserved() / 1e9:.2f} GB")
                 # Copy best model checkpoint to a predictable path + online tracker (if used)
                 # Ensure we use the best weights (and not the latest ones) by loading back the best model
                 model = model.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
