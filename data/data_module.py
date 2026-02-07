f745febd99088140a0aace22f45607cb443f6721
Jeremstym <jeremstym@gmail.com>
2026-02-07 01:56:14 +0100

    Set kwargs

--- data/data_module.py
+++ data/data_module.py
@@ -153,6 +153,7 @@ class TSPFNDataModule(pl.LightningDataModule):
         pin_memory: bool = True,
         transform: Optional[Callable] = None,
         seed: int = 42,
+        **kwargs,
     ) -> None:
         super().__init__()
         self.data_roots = data_roots
