ec4455a3f00ab536c61d9c4be15781cff4dea68a
Jeremstym <jeremstym@gmail.com>
2026-02-11 11:29:44 +0100

    Monitor

--- tspfn/pretraining/tspfn_module.py
+++ tspfn/pretraining/tspfn_module.py
@@ -389,12 +389,15 @@ class TSPFNPretraining(TSPFNSystem):
         else:
             if len(batch) == 3:
                 batch_dict, _, _ = batch
-                time_series_input, target_labels = batch_dict["query"]  # (N, C*T), (N,)
-                time_series_support, support_labels = batch_dict["support"]  # (N, C*T), (N,)
+                time_series_input, target_labels = batch_dict["query"]  # (N, C, T), (N,)
+                time_series_support, support_labels = batch_dict["support"]  # (N, C, T), (N,)
             elif type(batch) == dict:
                 # batch_dict, _, _ = batch
-                time_series_input, target_labels = batch["query"]  # (N, C*T), (N,)
-                time_series_support, support_labels = batch["support"]  # (N, C*T), (N,)
+                time_series_input, target_labels = batch["query"]  # (N, C, T), (N,)
+                time_series_support, support_labels = batch["support"]  # (N, C, T), (N,)
+
+        print(f"time_series_input shape: {time_series_input.shape}, target_labels shape: {target_labels.shape}")
+        raise Exception("Stop after checking batch shapes")
 
         y_batch_support, y_batch_query, ts_support, ts_query = self.process_data(
             time_series_attrs=time_series_input, labels=target_labels
