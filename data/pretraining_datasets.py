80045403515720ce3de67a94fb147391c2c6736d
Jeremstym <jeremstym@gmail.com>
2026-02-11 14:43:05 +0100

    Subsample PTB

--- data/pretraining_datasets.py
+++ data/pretraining_datasets.py
@@ -117,7 +117,12 @@ class PTB2ChannelDataset(Dataset):
         # self.files = glob(os.path.join(root, f"{split}/*.pkl"))
 
         self.X = np.load(os.path.join(root, f"{split}.npy"))
+        # Subsample time series
+        indices = range(len(self.X))
+        sub_indices = np.random.choice(indices, size=int(len(self.X) * 0.5), replace=False)
+        self.X = self.X[sub_indices]
         self.Y = np.load(os.path.join(root, f"{split}_label.npy"))
+        self.Y = self.Y[sub_indices]
         self.X = torch.from_numpy(self.X).float()
         self.X = self.X.reshape(self.X.shape[0], 2, -1)  # Reshape to [Batch, Channels, Signal_Length]
         self.Y = torch.from_numpy(self.Y).long().unsqueeze(1)  # Shape [Batch, 1]
@@ -238,7 +243,12 @@ class PTB3ChannelDataset(Dataset):
         self.root = root
         
         self.X = np.load(os.path.join(root, f"{split}.npy"))
+        # Subsample time series
+        indices = range(len(self.X))
+        sub_indices = np.random.choice(indices, size=int(len(self.X) * 0.5), replace=False)
+        self.X = self.X[sub_indices]
         self.Y = np.load(os.path.join(root, f"{split}_label.npy"))
+        self.Y = self.Y[sub_indices]
         self.X = torch.from_numpy(self.X).float()
         self.X = self.X.reshape(self.X.shape[0], 3, -1)  # Reshape to [Batch, Channels, Signal_Length]
         self.Y = torch.from_numpy(self.Y).long().unsqueeze(1)  # Shape [Batch, 1]
