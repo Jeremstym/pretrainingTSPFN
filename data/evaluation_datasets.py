a55ac067f30d792a8fd515eace9835f06722d3f4
Jeremstym <jeremstym@gmail.com>
2026-02-14 15:29:30 +0100

    Remove channels

--- data/evaluation_datasets.py
+++ data/evaluation_datasets.py
@@ -207,11 +207,11 @@ class ORCHIDDataset(Dataset):
         self.selected_channels = [
             "gls",
             "ls_left",
-            "ls_right",
+            # "ls_right",
             "lv_area",
-            "lv_length",
+            # "lv_length",
             "myo_thickness_left",
-            "myo_thickness_right",
+            # "myo_thickness_right",
         ]
 
         self.all_patients = sorted(glob(os.path.join(self.file_dir, "*.npz")))
