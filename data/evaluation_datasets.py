588bf6a497c2eb984e2dac45050b4bedf1820419
Jeremstym <jeremstym@gmail.com>
2026-02-18 19:29:05 +0100

    Activate all channels

--- data/evaluation_datasets.py
+++ data/evaluation_datasets.py
@@ -311,11 +311,11 @@ class EICUCRDDataset(Dataset):
         self.file_dir = os.path.join(self.root, f"{split}_decease")
         self.label_file = os.path.join(self.root, "final_labels.csv")
         self.selected_channels = [
-            # "heart_rate",
-            # "respiration",
-            # "spo2",
+            "heart_rate",
+            "respiration",
+            "spo2",
             "blood_pressure",
-            # "temperature"
+            "temperature"
         ]
 
         channel_maps = {
