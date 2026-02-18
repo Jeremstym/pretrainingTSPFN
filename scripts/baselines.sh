aa9ca0393a379560e898560dd8232591c498652a
Jeremstym <jeremstym@gmail.com>
2026-02-18 17:03:20 +0100

    Update baselines with TCN

--- scripts/baselines.sh
+++ scripts/baselines.sh
@@ -18,5 +18,5 @@ poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/orchid-xgboost' data=evaluating-orchid +experiment=baselines/xgboost seed=42 train=False test=True
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eicu-xgboost' data=evaluating-eicucrd +experiment=baselines/xgboost seed=42 train=False test=True
 
-# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-TCN/seed${seed}' +experiment=baselines/baseline seed=42 train=True test=True
-# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/esr-TCN/seed${seed}' +experiment=baselines/baseline data=finetuning-esr seed=42 train=True test=True
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-TCN/seed${seed}' +experiment=baselines/baseline seed=42 train=True test=True use_last=True +trainer.max_epochs=15 +trainer.limit_val_batches=0.0
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/esr-TCN/seed${seed}' +experiment=baselines/baseline data=finetuning-esr seed=42 train=True test=True use_last=True +trainer.max_epochs=15 +trainer.limit_val_batches=0.0
