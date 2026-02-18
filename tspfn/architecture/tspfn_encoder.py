2a35f0a16fcb3817df50fa4b7f9026f6fa19a6e8
Jeremstym <jeremstym@gmail.com>
2026-02-19 00:59:22 +0100

    Add extra 1 to fit dim

--- tspfn/architecture/tspfn_encoder.py
+++ tspfn/architecture/tspfn_encoder.py
@@ -244,7 +244,7 @@ class TSPFNEncoder(nn.Module, ABC):
             elif num_channels > 5:
                 # Repeat the learned positional encodings for more channels
                 channel_pe = self.cwpe.weight[:5, :].view(1, 1, 5, 1, self.embed_dim)  # (1, 1, 5, 1, E)
-                channel_pe = channel_pe.repeat(1, 1, num_channels // 5 + 1, 1)[:, :, :num_channels, :, :]  # (1, 1, C, 1, E)
+                channel_pe = channel_pe.repeat(1, 1, num_channels // 5 + 1, 1, 1)[:, :, :num_channels, :, :]  # (1, 1, C, 1, E)
             emb_x = emb_x + channel_pe  # Broadcast addition to (B, Seq, C, L, E) + (1, 1, C, 1, E)
             emb_x = emb_x.view(batch_size, seq_len, num_channels * num_features, self.embed_dim)  # (B, Seq, C*L, E)
             # if self.channel_positional_encoding is not None:
