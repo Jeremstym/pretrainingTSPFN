63ffd96caf30250c268f42a11367cb44f7789923
Jeremstym <jeremstym@gmail.com>
2026-02-19 00:41:38 +0100

    Preprocess eos dataset

---
+++ data/eos_preprocessing.py
@@ -0,0 +1,38 @@
+import os
+import csv
+import gzip
+import numpy as np
+import pandas as pd
+from sktime.datasets import load_from_tsfile
+from sktime.datatypes import convert_to
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+ORIGIN_DIRECTORY = "/data/stympopper/BenchmarkTSPFN/EOS/"
+OUTPUT_DIRECTORY = "/data/stympopper/BenchmarkTSPFN/processed/EyesOpenShut/"
+SPLIT_TRAIN = "train"
+SPLIT_TEST = "test"
+
+def process_eos_data(origin_dir, output_dir, split_train, split_test):
+    X_train, y_train = load_from_tsfile(os.path.join(origin_dir, "EyesOpenShut_TRAIN.ts"))
+    X_test, y_test = load_from_tsfile(os.path.join(origin_dir, "EyesOpenShut_TEST.ts"))
+
+    X_train_numpy = convert_to(X_train, "numpy3D")
+    X_test_numpy = convert_to(X_test, "numpy3D")
+
+
+    os.makedirs(output_dir, exist_ok=True)
+    np.save(os.path.join(output_dir, f"{split_train}_features.npy"), X_train_numpy)
+    np.save(os.path.join(output_dir, f"{split_train}_labels.npy"), y_train.astype(float))
+    np.save(os.path.join(output_dir, f"{split_test}_features.npy"), X_test_numpy)
+    np.save(os.path.join(output_dir, f"{split_test}_labels.npy"), y_test.astype(float))
+
+    return
+
+if __name__ == "__main__":
+    process_eos_data(
+        origin_dir=ORIGIN_DIRECTORY,
+        output_dir=OUTPUT_DIRECTORY,
+        split_train=SPLIT_TRAIN,
+        split_test=SPLIT_TEST,
+    )
