edcfb7e14300c443118553844b920636f7657381
Jeremstym <jeremstym@gmail.com>
2026-02-19 01:09:06 +0100

    Keep four channels

--- data/evaluation_datasets.py
+++ data/evaluation_datasets.py
@@ -397,7 +397,7 @@ class EOSDataset(Dataset):
         if self.X.shape[1] > 5:
             # Use the first 5 channels if there are more than 5
             # keep_channels = [0, 10, 11, 12, 13]
-            self.X = self.X[:, :5, :]
+            self.X = self.X[:, :4, :]
 
     def __len__(self):
         return len(self.X)
