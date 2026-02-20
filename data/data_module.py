6264aa2d27ff5004211b888363a1e60e493efce7
Jeremstym <jeremstym@gmail.com>
2026-02-20 16:43:28 +0100

    Add folding in evaluting eos

--- data/data_module.py
+++ data/data_module.py
@@ -634,6 +634,8 @@ class EOSDataModule(TSPFNDataModule):
         num_workers: int = 0,
         batch_size: int = 32,
         test_batch_size: Optional[int] = None,
+        support_size: Optional[int] = None,
+        fold: Optional[int] = None,
         pin_memory: bool = True,
         transform: Optional[Callable] = None,
         seed: int = 42,
@@ -645,6 +647,8 @@ class EOSDataModule(TSPFNDataModule):
             num_workers=num_workers,
             batch_size=batch_size,
             test_batch_size=test_batch_size,
+            support_size=support_size,
+            fold=fold,
             pin_memory=pin_memory,
             transform=transform,
             seed=seed,
@@ -657,6 +661,8 @@ class EOSDataModule(TSPFNDataModule):
         self.train_dataset = EOSDataset(
             root=self.data_roots,
             split="train",
+            support_size=self.support_size,
+            fold=self.fold,
         )
         # scaler = self.train_dataset.scaler
         self.val_dataset = EOSDataset(root=self.data_roots, split="test")
