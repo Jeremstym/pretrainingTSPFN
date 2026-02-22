f8e459dd51d349ca888cc670c0e0e736674e212d
Jeremstym <jeremstym@gmail.com>
2026-02-22 02:17:12 +0100

    Configure patch tst

--- scripts/baselines.sh
+++ scripts/baselines.sh
@@ -25,6 +25,8 @@ poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eicu-xgb
 poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eicu-TCN-full/seed${seed}' +experiment=baselines/baseline data=finetuning-eicucrd seed=42 train=True test=True use_last=True trainer.max_epochs=15 +trainer.limit_val_batches=0.0
 poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eos-TCN-full/seed${seed}' +experiment=baselines/baseline data=finetuning-eos seed=42 train=True test=True use_last=True trainer.max_epochs=15 +trainer.limit_val_batches=0.0
 
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-patchTST-full/seed${seed}' +experiment=baselines/baseline data=finetuning-ecg5000 seed=42 train=True test=True use_last=True trainer.max_epochs=15 +trainer.limit_val_batches=0.0
+
 for fold in 0 1 2 3 4; do
     poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-TCN/fold${fold}' +experiment=baselines/baseline data=finetuning-ecg5000 seed=42 train=True test=True use_last=True trainer.max_epochs=15 +trainer.limit_val_batches=0.0 data.support_size=500 data.fold=${fold} +fold=${fold}
   done
