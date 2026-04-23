3a47363cadb3a214f35c4afe9a320d7f464ebea0
Jeremstym <jeremstym@gmail.com>
2026-04-23 16:47:33 +0200

    Back to electronic

--- scripts/mantis_pretrain.sh
+++ scripts/mantis_pretrain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=hard
-###SBATCH --nodelist=punk
+#SBATCH --partition=electronic
+#SBATCH --nodelist=punk
 #SBATCH --job-name=Mantis-pretraining
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
