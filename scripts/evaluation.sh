1d1d1ed382ab82f7f71c4b6d05c4ca9b590e91f4
Jeremstym <jeremstym@gmail.com>
2026-04-27 15:15:21 +0200

    Update evaluation script to include additional pretraining commands for various datasets

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -11,4 +11,8 @@
 uname -a
 nvidia-smi
 
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/mantis-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-ecg5000 seed=42 train=False test=True
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/ecg5000-mantis-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-ecg5000 seed=42 train=False test=True
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/esr-mantis-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-esr seed=42 train=False test=True
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eos-mantis-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-eos seed=42 train=False test=True
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eicu-mantis-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-eicucrd seed=42 train=False test=True
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/cpsc-mantis-tabpfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-cpsc seed=42 train=False test=True
