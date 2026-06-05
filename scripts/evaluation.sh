ec414a75439aa05adb534bf7e5d24e95e523d491
Jeremstym <jeremstym@gmail.com>
2026-06-05 17:10:13 +0200

    Update job name in evaluation script and add evaluation command for TabPFN v3

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --nodelist=kavinsky
 #SBATCH --gpus-per-node=1
-#SBATCH --job-name=Mantis-tokenizer
+#SBATCH --job-name=TabPFN-v3-evaluation
 #SBATCH --time=1-23:00:00
 #SBATCH --output=/home/stympopper/bash/out/%x-%j.out
 #SBATCH --error=/home/stympopper/bash/out/%x-%j.err
@@ -21,4 +21,7 @@ nvidia-smi
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/esr-mantis-tspfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-esr seed=42 train=False test=True updated_pfn_path="/home/stympopper/pretrainingTSPFN/ckpts/TSPFN-Mantis.pt"
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eos-mantis-tspfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-eos seed=42 train=False test=True updated_pfn_path="/home/stympopper/pretrainingTSPFN/ckpts/TSPFN-Mantis.pt"
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/eicu-mantis-tspfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-eicucrd seed=42 train=False test=True updated_pfn_path="/home/stympopper/pretrainingTSPFN/ckpts/TSPFN-Mantis.pt"
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/cpsc-mantis-tspfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-cpsc seed=42 train=False test=True updated_pfn_path="/home/stympopper/pretrainingTSPFN/ckpts/TSPFN-Mantis.pt"
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/cpsc-mantis-tspfn/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-cpsc seed=42 train=False test=True updated_pfn_path="/home/stympopper/pretrainingTSPFN/ckpts/TSPFN-Mantis.pt"
+
+#! EVALUATING TabPFN v3 below
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/cpsc-tspfn3/seed${seed}' +experiment=finetuningTSPFN/tspfn3-finetuning data=evaluating-cpsc seed=42 train=False test=True
