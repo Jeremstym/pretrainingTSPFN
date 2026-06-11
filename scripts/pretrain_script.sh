61b44d32d4d88aa20695decef323a485c96a2d8e
Jeremstym <jeremstym@gmail.com>
2026-06-11 16:29:56 +0200

    Update pretrain_script.sh to add pretraining command for TabPFN v3 with AttentionChannelCLS

--- scripts/pretrain_script.sh
+++ scripts/pretrain_script.sh
@@ -56,5 +56,5 @@ nvidia-smi
 #! Pretraining CubePFN for TabPFN v3
 
 #? poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN-v3-AttentionChannel-CLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42 task.model.encoder.use_checkpoint=False
-# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN-v3-AttentionChannelCLS-AvgCLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
+#? poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42 task.model.encoder.use_checkpoint=False
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN-v3-AttentionChannelCLS-AvgCLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
