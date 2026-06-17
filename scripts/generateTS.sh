9efe0974779305bd68994456a2fa0b6ab6abd18f
Jeremstym <jeremstym@gmail.com>
2026-06-17 09:36:25 +0200

    Add environment variable exports for CUDA in generateTS.sh

--- scripts/generateTS.sh
+++ scripts/generateTS.sh
@@ -11,4 +11,8 @@ uname -a
 nvidia-smi
 ulimit -n 4096
 
+export CUDA_PATH=/usr/local/cuda-11.1
+export PATH=$CUDA_PATH/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
+
 poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
