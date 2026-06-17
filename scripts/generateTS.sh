2631049c9dea96e9c00440a62057d0d33048d816
Jeremstym <jeremstym@gmail.com>
2026-06-17 09:41:07 +0200

    Fix CUDA module loading and update working directory path in generateTS.sh

--- scripts/generateTS.sh
+++ scripts/generateTS.sh
@@ -11,10 +11,12 @@ uname -a
 nvidia-smi
 ulimit -n 4096
 
+module load cuda/11.1 2>/dev/null || module load cuda/11.1.1 2>/dev/null
+
 export CUDA_PATH=/usr/local/cuda-11.1
 export PATH=$CUDA_PATH/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
 
-cd pretrainingTSPFN
+cd /home/stympopper/pretrainingTSPFN
 
 poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
