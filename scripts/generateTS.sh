5f4aa3c59583e50d02ea5e36e4eaa59c3b7e9ff9
Jeremstym <jeremstym@gmail.com>
2026-06-17 09:57:26 +0200

    Remove commented-out CUDA module loading and streamline script execution in generateTS.sh

--- scripts/generateTS.sh
+++ scripts/generateTS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=electronic
+#SBATCH --nodelist=punk
 #SBATCH --job-name=CauKer-generating
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
@@ -10,14 +11,5 @@
 uname -a
 nvidia-smi
 ulimit -n 4096
-/home/stympopper/.cache/pypoetry/virtualenvs/tspfn-_Zk-uwh--py3.10/bin/python /home/stympopper/CauKer/CauKer.py
 
-# module load cuda/11.1 2>/dev/null || module load cuda/11.1.1 2>/dev/null
-
-# export CUDA_PATH=/usr/local/cuda-11.1
-# export PATH=$CUDA_PATH/bin:$PATH
-# export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
-
-# cd /home/stympopper/pretrainingTSPFN
-
-# poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
+poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
