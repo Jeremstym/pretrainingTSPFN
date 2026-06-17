a0933a1a88962c8d88a7016947abef3650fc40ec
Jeremstym <jeremstym@gmail.com>
2026-06-17 09:39:29 +0200

    Fix directory change in generateTS.sh to ensure correct script execution

--- scripts/generateTS.sh
+++ scripts/generateTS.sh
@@ -15,4 +15,6 @@ export CUDA_PATH=/usr/local/cuda-11.1
 export PATH=$CUDA_PATH/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
 
+cd pretrainingTSPFN
+
 poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
