73d7499f8449ff5aa50fd868b8ca2a855e32d522
Jeremstym <jeremstym@gmail.com>
2026-06-17 09:33:20 +0200

    Add generateTS.sh script for CauKer job submission

---
+++ scripts/generateTS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=electronic
+#SBATCH --job-name=CauKer-generating
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=1-23:00:00
+#SBATCH --output=/home/stympopper/bash/out/%x-%j.out
+#SBATCH --error=/home/stympopper/bash/out/%x-%j.err
+
+uname -a
+nvidia-smi
+ulimit -n 4096
+
+poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
