cc32922f1f9509df836b0f309490b4c07d03485d
Jeremstym <jeremstym@gmail.com>
2026-06-17 09:44:16 +0200

    Comment out CUDA module loading and environment variable exports in generateTS.sh

--- scripts/generateTS.sh
+++ scripts/generateTS.sh
@@ -10,14 +10,14 @@
 uname -a
 nvidia-smi
 ulimit -n 4096
+/home/stympopper/.cache/pypoetry/virtualenvs/tspfn-_Zk-uwh--py3.10/bin/python /home/stympopper/CauKer/CauKer.py
 
-module load cuda/11.1 2>/dev/null || module load cuda/11.1.1 2>/dev/null
+# module load cuda/11.1 2>/dev/null || module load cuda/11.1.1 2>/dev/null
 
-export CUDA_PATH=/usr/local/cuda-11.1
-export PATH=$CUDA_PATH/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
+# export CUDA_PATH=/usr/local/cuda-11.1
+# export PATH=$CUDA_PATH/bin:$PATH
+# export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$LD_LIBRARY_PATH
 
-cd /home/stympopper/pretrainingTSPFN
+# cd /home/stympopper/pretrainingTSPFN
 
 # poetry run python /home/stympopper/CauKer/CauKer.py -N 2000000 -L 512 -F 4 -P 6 -M 18 -O /data/stympopper/CauKer2M/CauKer2M.arrow
-/home/stympopper/.cache/pypoetry/virtualenvs/tspfn-_Zk-uwh--py3.10/bin/python /home/stympopper/CauKer/CauKer.py
