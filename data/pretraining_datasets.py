8e1a938bb1a94fe02130907b16ba9f3895929de5
Jeremstym <jeremstym@gmail.com>
2026-06-19 14:59:49 +0200

    Import train test split from sklearn

--- data/pretraining_datasets.py
+++ data/pretraining_datasets.py
@@ -11,6 +11,7 @@ import pandas as pd
 from scipy.io import arff
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from pathlib import Path
 import argparse
 from typing import Dict, List, Tuple, Union, Any
