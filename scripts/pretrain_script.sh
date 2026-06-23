5c8a596d02ccbd9974de2527b12d5d2898b57590
Jeremstym <jeremstym@gmail.com>
2026-06-23 18:40:50 +0200

    Add Mantis pretraining script and comment out noFFT variant

--- scripts/pretrain_script.sh
+++ scripts/pretrain_script.sh
@@ -62,4 +62,6 @@ nvidia-smi
 
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN-SSL-Cauker2M' +experiment=pretrainingTSPFN/cubepfn3-contrastive-pretraining seed=42
 #? poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN-SSL-Cauker100K' +experiment=pretrainingTSPFN/cubepfn3-contrastive-pretraining seed=42
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN-SSL-Cauker100K-noFFT' +experiment=pretrainingTSPFN/cubepfn3-contrastive-pretraining seed=42
+#? poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN-SSL-Cauker100K-noFFT' +experiment=pretrainingTSPFN/cubepfn3-contrastive-pretraining seed=42
+
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN-MantisPretraining-FFT' +experiment=pretrainingTSPFN/cubepfn3-mantis-pretraining seed=42
