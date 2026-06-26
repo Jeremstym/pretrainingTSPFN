a92b3d329b7c595f5dce5a0c43d06ac357eea759
Jeremstym <jeremstym@gmail.com>
2026-06-26 10:13:10 +0200

    Add new benchmark to test mantis cubepfn pretrained

--- scripts/newbenchmark.sh
+++ scripts/newbenchmark.sh
@@ -150,8 +150,8 @@ UCR2019_univariate=(
 
 for dataset in "${UCR2019_univariate[@]}"; do
     poetry run tspfn-pretrain \
-        "hydra.run.dir=/data/stympopper/TSPFN-Benchmark/UCRUnivariate/${dataset}-CubePFN3-CauKer100K/seed\${seed}" \
-        +experiment=finetuningTSPFN/tspfn3-finetuning \
+        "hydra.run.dir=/data/stympopper/TSPFN-Benchmark/UCRUnivariate/${dataset}-CubePFN-mantis-CauKer2M/seed\${seed}" \
+        +experiment=finetuningTSPFN/cubepfn3-finetuning \
         data=benchmark/evaluating-ucrunivariate \
         data.dataset="$dataset" \
         task.adaptable_metrics=True \
@@ -159,9 +159,24 @@ for dataset in "${UCR2019_univariate[@]}"; do
         +dataset="$dataset" \
         train=False \
         test=True \
-        ckpt="/home/stympopper/pretrainingTSPFN/ckpts/cubePFN-pretrained-cauker100K.ckpt" \
+        ckpt="/home/stympopper/pretrainingTSPFN/ckpts/CubePFN-mantis-pretrained.ckpt" \
         strict=False
 done
+
+# for dataset in "${UCR2019_univariate[@]}"; do
+#     poetry run tspfn-pretrain \
+#         "hydra.run.dir=/data/stympopper/TSPFN-Benchmark/UCRUnivariate/${dataset}-CubePFN3-CauKer100K/seed\${seed}" \
+#         +experiment=finetuningTSPFN/tspfn3-finetuning \
+#         data=benchmark/evaluating-ucrunivariate \
+#         data.dataset="$dataset" \
+#         task.adaptable_metrics=True \
+#         seed=42 \
+#         +dataset="$dataset" \
+#         train=False \
+#         test=True \
+#         ckpt="/home/stympopper/pretrainingTSPFN/ckpts/cubePFN-pretrained-cauker100K.ckpt" \
+#         strict=False
+# done
 
 # for dataset in "${UCR2019_univariate[@]}"; do
 #     poetry run tspfn-pretrain \
