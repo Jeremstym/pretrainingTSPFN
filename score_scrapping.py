4aef323f183419265dbc8253991f227bfb36f0c6
Jeremstym <jeremstym@gmail.com>
2026-07-06 11:30:05 +0200

    Update main_v3 to adjust results directory and output file naming for Mantis variant

--- score_scrapping.py
+++ score_scrapping.py
@@ -114,7 +114,7 @@ def main():
 
 def main_v3():
     # Define the path to the directory containing the results
-    results_dir = Path("/data/stympopper/TSPFN-Benchmark/UCRUnivariate").glob("*-CubePFN3-Mantis-NoDiff*")
+    results_dir = Path("/data/stympopper/TSPFN-Benchmark/UCRUnivariate").glob("*-CubePFN3-Mantis*")
 
     # Initialize a list to store the results
     results = []
@@ -152,8 +152,8 @@ def main_v3():
     results_df = pd.DataFrame(results).sort_values(by=["dataset", "seed"])
     # results_df = filter_datasets_by_class_count(results_df)
     output_dir = "/data/stympopper/TSPFN-Benchmark"
-    results_df.to_csv(f"{output_dir}/less_10_ucr_univariate_results_summary-CubePFN3-Mantis-NoDiff.csv", index=False)
-    print("Results summary saved to less_10_ucr_univariate_results_summary-CubePFN3-Mantis-NoDiff.csv")
+    results_df.to_csv(f"{output_dir}/less_10_ucr_univariate_results_summary-CubePFN3-Mantis.csv", index=False)
+    print("Results summary saved to less_10_ucr_univariate_results_summary-CubePFN3-Mantis.csv")
 
 def main_mantis_rf():
     # Define the path to the directory containing the results
