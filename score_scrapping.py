d27f53e5c0e7eba4726b062baff8c28b0369722d
Jeremstym <jeremstym@gmail.com>
2026-07-06 11:29:24 +0200

    Update main_v3 to change results directory and output file naming to NoDiff variant

--- score_scrapping.py
+++ score_scrapping.py
@@ -114,7 +114,7 @@ def main():
 
 def main_v3():
     # Define the path to the directory containing the results
-    results_dir = Path("/data/stympopper/TSPFN-Benchmark/UCRUnivariate").glob("*-CubePFN3-Mantis-FineTune*")
+    results_dir = Path("/data/stympopper/TSPFN-Benchmark/UCRUnivariate").glob("*-CubePFN3-Mantis-NoDiff*")
 
     # Initialize a list to store the results
     results = []
@@ -152,8 +152,8 @@ def main_v3():
     results_df = pd.DataFrame(results).sort_values(by=["dataset", "seed"])
     # results_df = filter_datasets_by_class_count(results_df)
     output_dir = "/data/stympopper/TSPFN-Benchmark"
-    results_df.to_csv(f"{output_dir}/less_10_ucr_univariate_results_summary-CubePFN3-Mantis-FineTune.csv", index=False)
-    print("Results summary saved to less_10_ucr_univariate_results_summary-CubePFN3-Mantis-FineTune.csv")
+    results_df.to_csv(f"{output_dir}/less_10_ucr_univariate_results_summary-CubePFN3-Mantis-NoDiff.csv", index=False)
+    print("Results summary saved to less_10_ucr_univariate_results_summary-CubePFN3-Mantis-NoDiff.csv")
 
 def main_mantis_rf():
     # Define the path to the directory containing the results
