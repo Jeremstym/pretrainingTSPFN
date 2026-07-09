10bb0019a238c29c0095f34a8d7d6d47bafa2af3
Jeremstym <jeremstym@gmail.com>
2026-07-09 17:38:29 +0200

    Initialize test_predictions_storage attribute in training and validation start methods

--- tspfn/finetuning/tspfn_finetune.py
+++ tspfn/finetuning/tspfn_finetune.py
@@ -663,6 +663,14 @@ class TSPFNFineTuning(TSPFNSystem):
 
         return metrics
 
+    def on_train_start(self):
+        # Initialize a custom attribute to store predictions
+        self.test_predictions_storage = []
+
+    def on_validation_start(self):
+        # Initialize a custom attribute to store predictions
+        self.test_predictions_storage = []
+
     def on_test_start(self):
         # Initialize a custom attribute to store predictions
         self.test_predictions_storage = []
