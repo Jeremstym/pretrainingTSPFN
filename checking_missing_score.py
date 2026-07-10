113ac0b4de6cbda66c7c37a1745c7ddb452952f4
Jeremstym <jeremstym@gmail.com>
2026-07-10 18:18:15 +0200

    Add check_scores function to identify missing score files in specified directories

---
+++ checking_missing_score.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+import shutil
+from glob import glob
+
+
+def check_scores(path: Path, pattern: str = "CubePFN3-FineTune"):
+    """
+    Check for missing scores in the given path.
+    """
+    missing_files = []
+    for folder in os.listdir(path):
+        if folder.endswith(pattern):
+            print(f"Checking scores in {folder}")
+            if not any(glob(os.path.join(path, folder, "**", "test_metrics.csv"), recursive=True)):
+                missing_files.append(folder)
+    if missing_files:
+        print("Missing scores found in the following directories:")
+        for missing in missing_files:
+            print(missing)
+
+if __name__ == "__main__":
+    path = "/data/stympopper/TSPFN-Benchmark/UCRUnivariate"
+    check_scores(path, pattern="-CubePFN3-FineTune")
