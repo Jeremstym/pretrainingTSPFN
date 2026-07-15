404fafc34b08f746d8398242ad0ad044bb9221c2
Jeremstym <jeremstym@gmail.com>
2026-07-15 14:20:01 +0200

    Fix typo in directory name for CubePFN pretraining command

--- scripts/pretrain_script.sh
+++ scripts/pretrain_script.sh
@@ -60,7 +60,7 @@ poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN3-Med
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42 task.model.encoder.use_checkpoint=False
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS-part2' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42 task.model.encoder.use_checkpoint=False ckpt="/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS/checkpoints/epoch\=100-step\=1515.ckpt"
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN3-MedPretrained' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TRAHS-TEST' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TRASH-TEST' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN3-Reg-MedPretrained' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
 
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN-SSL-Cauker2M' +experiment=pretrainingTSPFN/cubepfn3-contrastive-pretraining seed=42
