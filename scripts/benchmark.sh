b4aca378e32e76f0fc25ffefca094056d8ee8c33
Jeremstym <jeremstym@gmail.com>
2026-07-15 15:20:49 +0200

    Add evaluating-orchid.yaml configuration and update benchmark script for Mantis fine-tuning

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -116,5 +116,15 @@ poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/cpsc-TSP
 poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/cpsc-TSPFNFM-RoPE+CWPE-zscoring-shuffle-5chans+hirid-v2/seed${seed}' +experiment=finetuningTSPFN/tspfn-finetuning data=evaluating-cpsc seed=42 train=False test=True updated_pfn_path="/home/stympopper/pretrainingTSPFN/ckpts/TSPFN-RoPE+CWPE-zscoring-5CHANS+hirid-nowarmup-shuffle-2.pt" task.time_series_positional_encoding=cwpe+rope
 
 #! EVALUATE MANTIS on ORCHID
+# for fold in 0 1 2 3 4; do
+#     for seed in 42; do
+#         poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/orchid-Mantis-RF/seed${seed}/fold${fold}' +experiment=baselines/mantis_v2 data=evaluating-orchid data.mantis_training=True 'data.fold=${fold}' seed=42 train=False test=True +fold=$fold
+#     done
+# done
+for fold in 0 1 2 3 4; do
+    for seed in 42; do
+        poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/orchid-Mantis-FineTune/seed${seed}/fold${fold}' +experiment=baselines/mantis_v2 data=evaluating-orchid data.mantis_training=True task.finetuning=True 'data.fold=${fold}' seed=42 train=False test=True +fold=$fold
+    done
+done
 
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/orchid-Mantis-RF/seed${seed}/fold${fold}' +experiment=baselines/mantis_v2 data=evaluating-orchid data.mantis_training=True data.fold=0 seed=42 train=False test=True +fold=0
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/TSPFN_results/orchid-Mantis-RF/seed${seed}/fold${fold}' +experiment=baselines/mantis_v2 data=evaluating-orchid data.mantis_training=True data.fold=1 seed=42 train=False test=True +fold=1
