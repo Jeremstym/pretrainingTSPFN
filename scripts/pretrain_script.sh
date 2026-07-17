8d628d229df25071e14a3bccb4e93c239acdbab7
Jeremstym <jeremstym@gmail.com>
2026-07-17 21:08:07 +0200

    Update l2 regularizer coefficient and modify pretraining script for new directory

--- scripts/pretrain_script.sh
+++ scripts/pretrain_script.sh
@@ -56,7 +56,8 @@ nvidia-smi
 #! Pretraining CubePFN for TabPFN v3
 
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN-v3-AttentionChannel-CLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
-poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN3-MedPretrained' +experiment=pretrainingTSPFN/tspfn3-pretraining seed=42
+# poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN3-MedPretrained' +experiment=pretrainingTSPFN/tspfn3-pretraining seed=42
+poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabPFN3-Reg-MedPretrained' +experiment=pretrainingTSPFN/tspfn3-pretraining seed=42
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42 task.model.encoder.use_checkpoint=False
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS-part2' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42 task.model.encoder.use_checkpoint=False ckpt="/data/stympopper/CubePFN_FM/TabICL-v3-AttentionChannelCLS-AvgCLS/checkpoints/epoch\=100-step\=1515.ckpt"
 # poetry run tspfn-pretrain 'hydra.run.dir=/data/stympopper/CubePFN_FM/CubePFN3-MedPretrained' +experiment=pretrainingTSPFN/cubepfn3-pretraining seed=42
